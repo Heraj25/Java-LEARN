757679281a675a510be42397408e1ab14d7d7044
Heraj25 <herajgantyada@gmail.com>
2023-02-11 10:51:25 +0530

    sixth commit

---
+++ Firstclass/fibinocci.java
@@ -0,0 +1,25 @@
+package Firstclass;
+
+import java.util.Scanner;
+public class fibinocci {
+    public static void main(String[] args) {
+        
+    
+    int n;
+    Scanner sc = new Scanner(System.in);
+    int a=0;
+    int b=1;
+    n = sc.nextInt();
+    int sum;
+    sc.close();
+    System.out.println(a);
+    System.out.println(b);
+    for(int i=1 ; i<=n ; i++){
+        sum = a+b;
+        a = b;
+        b = sum;
+        System.out.println(sum);
+    }
+    
+}
+}
